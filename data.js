d657d2db9bad40619947f1bc231ccce85d1dd769
naiadidub <antonio.zaghi1405@gmail.com>
2022-04-11 17:34:10 +0200

    nel mezzo del terzo milestone

--- data.js
+++ data.js
@@ -115,13 +115,38 @@ const elenco = [
 
 //Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 function creaCard (){
-	while(i < elenco.length){
+	let container = document.getElementById('container')
+	let row = document.getElementById('row')
+	for(let i = 0; i < elenco.length; i++){
 		let card = document.createElement('div')
-		card = `<div class="col-2 square rounded">
-				<i class="fa-solid fa-${elenco[i].name}">
+		card.innerHTML += `<div class="col-2 square rounded">
+				<i class="fa-solid fa-${elenco[i].name} color-${elenco[i].color}">
 					<p class="sub">${elenco[i].name}</p>
 
 				</i>
 			</div>`
+		row.append(card)
+		container.append(row)
 	}
 }
+creaCard()
+
+let select = document.getElementById('level');
+let scelta = select.options[select.selectedIndex].value;
+console.log(scelta)
+switch(scelta) {
+	case 'animal':
+	  // code block
+	  scelta == 'animal'
+	  break;
+	case 'vegetable':
+	  // code block
+	  scelta == 'vegetable'
+	  break;
+	case 'user':
+	  // code block
+	  scelta == 'user'
+	  break;  
+	default:
+	  // code block
+  }
